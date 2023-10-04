90a8c557ac0953ad2840ccc912b058bc44837926
RokyXxX <goofboiiahh@gmail.com>
2023-10-04 08:29:41 +0530

    Add `fs`, change some configurity values..

---
+++ types/.template.ts
@@ -0,0 +1,19 @@
+// TEMPLATE for any file.
+
+const startYear = new Date().getFullYear();
+const nextYear = new Date().getFullYear() + 1;
+let version = "1.0.0";
+
+const tsTemplate = `
+/** NebulaKit - ts (TypeScript)
+ * @authors The NebulaKit Developer(s) ${startYear}-${nextYear}
+ * @version ${version}
+ * **/
+`;
+
+const scssTemplate = `
+/** NebulaKit - scss (SCSS)
+ * @authors The NebulaKit Developer(s) ${startYear}-${nextYear}
+ * @version ${version}
+ * **/
+`;
